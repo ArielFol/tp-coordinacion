14bfc8d708e895ebce0ce7768ab22b3f87f05240
FrancoBar <fbarreneche@fi.uba.ar>
2026-04-09 20:56:03 -0300

    refactor: use struct functions and separate package in join

--- golang/src/join/main.go
+++ golang/src/join/main.go
@@ -1,82 +1,90 @@
 package main
 
 import (
-	"log"
+	"errors"
+	"log/slog"
 	"os"
 	"strconv"
 
-	"github.com/7574-sistemas-distribuidos/tp-coordinacion/common/middleware"
+	"github.com/7574-sistemas-distribuidos/tp-coordinacion/join"
 )
 
-const MOM_PORT = 5672
-
-type Join struct {
-	MomHost           string
-	InputQueue        string
-	OutputQueue       string
-	SumAmount         int
-	SumPrefix         string
-	AggregationAmount int
-	AggregationPrefix string
-	TopSize           int
-}
-
-func loadConfig() (*Join, error) {
+func loadConfig() (join.JoinConfig, error) {
 	sumAmount, err := strconv.Atoi(os.Getenv("SUM_AMOUNT"))
 	if err != nil {
-		return nil, err
+		return join.JoinConfig{}, err
 	}
 
 	aggregationAmount, err := strconv.Atoi(os.Getenv("AGGREGATION_AMOUNT"))
 	if err != nil {
-		return nil, err
+		return join.JoinConfig{}, err
 	}
 
 	topSize, err := strconv.Atoi(os.Getenv("TOP_SIZE"))
 	if err != nil {
-		return nil, err
+		return join.JoinConfig{}, err
 	}
 
-	joinConfig := Join{
-		MomHost:           os.Getenv("MOM_HOST"),
-		InputQueue:        os.Getenv("INPUT_QUEUE"),
-		OutputQueue:       os.Getenv("OUTPUT_QUEUE"),
+	momPort, err := strconv.Atoi(os.Getenv("MOM_PORT"))
+	if err != nil {
+		return join.JoinConfig{}, errors.New("MOM_PORT environment variable is required and must be a number")
+	}
+
+	momHost := os.Getenv("MOM_HOST")
+	if momHost == "" {
+		return join.JoinConfig{}, errors.New("MOM_HOST environment variable is required")
+	}
+
+	inputQueue := os.Getenv("INPUT_QUEUE")
+	if inputQueue == "" {
+		return join.JoinConfig{}, errors.New("INPUT_QUEUE environment variable is required")
+	}
+
+	outputQueue := os.Getenv("OUTPUT_QUEUE")
+	if outputQueue == "" {
+		return join.JoinConfig{}, errors.New("OUTPUT_QUEUE environment variable is required")
+	}
+
+	sumPrefix := os.Getenv("SUM_PREFIX")
+	if sumPrefix == "" {
+		return join.JoinConfig{}, errors.New("SUM_PREFIX environment variable is required")
+	}
+
+	aggregationPrefix := os.Getenv("AGGREGATION_PREFIX")
+	if aggregationPrefix == "" {
+		return join.JoinConfig{}, errors.New("AGGREGATION_PREFIX environment variable is required")
+	}
+
+	return join.JoinConfig{
+		MomHost:           momHost,
+		MomPort:           momPort,
+		InputQueue:        inputQueue,
+		OutputQueue:       outputQueue,
 		SumAmount:         sumAmount,
-		SumPrefix:         os.Getenv("SUM_PREFIX"),
+		SumPrefix:         sumPrefix,
 		AggregationAmount: aggregationAmount,
-		AggregationPrefix: os.Getenv("AGGREGATION_PREFIX"),
+		AggregationPrefix: aggregationPrefix,
 		TopSize:           topSize,
-	}
-
-	return &joinConfig, nil
+	}, nil
 }
 
-func handleMessage(outputQueue middleware.Middleware, msg middleware.Message, ack func(), nack func()) {
-	defer ack()
-	if err := outputQueue.Send(msg); err != nil {
-		log.Println("While sending top", err)
+func run() int {
+	config, err := loadConfig()
+	if err != nil {
+		slog.Error("While loading config", "err", err)
+		return 1
 	}
+
+	server, err := join.NewJoin(config)
+	if err != nil {
+		slog.Error("While initializing join", "err", err)
+		return 1
+	}
+
+	server.Run()
+	return 0
 }
 
 func main() {
-	joinConfig, err := loadConfig()
-	if err != nil {
-		log.Println("While reading config", err)
-		return
-	}
-	connectionConfig := middleware.ConnSettings{Hostname: joinConfig.MomHost, Port: MOM_PORT}
-	inputQueue, err := middleware.CreateQueueMiddleware(joinConfig.InputQueue, connectionConfig)
-	if err != nil {
-		log.Println("While creating input queue", err)
-		return
-	}
-	outputQueue, err := middleware.CreateQueueMiddleware(joinConfig.OutputQueue, connectionConfig)
-	if err != nil {
-		log.Println("While creating output queue", err)
-		return
-	}
-
-	inputQueue.StartConsuming(func(msg middleware.Message, ack, nack func()) {
-		handleMessage(outputQueue, msg, ack, nack)
-	})
+	os.Exit(run())
 }
